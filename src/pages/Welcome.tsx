3b6631044bf88ce6de7282118da58e3cf0e90b1d
quqiufeng <quqiufeng@gmail.com>
2021-02-13 12:12:37 +0800

    组件的其他写法&&组件多语言实现

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -12,7 +12,8 @@ const CodePreview: React.FC = ({ children }) => (
   </pre>
 );
 
-export default (): React.ReactNode => {
+//export default (): React.ReactNode => {
+const Welcome: React.FC = () => {
   const intl = useIntl();
   return (
     <PageContainer>
@@ -61,3 +62,5 @@ export default (): React.ReactNode => {
     </PageContainer>
   );
 };
+
+export default Welcome;
--- src/pages/exception/500/index.tsx
+++ src/pages/exception/500/index.tsx
@@ -1,19 +1,33 @@
 import { Link } from 'umi';
 import { Result, Button } from 'antd';
 import React from 'react';
+import language from './locales/zh-CN';
+import { useIntl } from 'umi';
 
-export default () => (
+const Status500: React.FC = () => {
+  const intl = useIntl();
+  return  (
   <Result
     status="500"
     title="500"
     style={{
       background: 'none',
     }}
-    subTitle="Sorry, the server is reporting an error."
+    subTitle={intl.formatMessage({
+      id: 'exceptionand500.description.500',
+      defaultMessage: 'Sorry, the server is reporting an error.',
+    })}
     extra={
       <Link to="/">
-        <Button type="primary">Back Home</Button>
+        <Button type="primary">{
+          intl.formatMessage({
+            id: 'exceptionand500.exception.back',
+            defaultMessage: 'Back Home',
+          })
+        }</Button>
       </Link>
     }
   />
-);
+  )
+}
+export default Status500;
